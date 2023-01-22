2bdec60b7cf27a0fd13570b7ac848ad83a66cc94
Jonathan Dao <jonathandao0@gmail.com>
2023-01-21 21:09:35 -0800

    Updated install script

--- install_scripts/rockpi_setup.sh
+++ install_scripts/rockpi_setup.sh
@@ -1,10 +1,7 @@
-echo "" >> ~/.bashrc
-echo "alias ntpup='sudo service ntp stop; sudo ntpd -qa; sudo ntp service start'" >> ~/.bashrc
-source ~/.bashrc
-
 sudo apt-get -y update
 sudo apt-get -y install python-opencv
 sudo apt-get -y install python3-pyqt5
 sudo apt-get -y install usbtop
 sudo apt-get -y install vnstat
 sudo wget -qO- https://docs.luxonis.com/install_dependencies.sh | bash
+pip3 install --find-links=https://tortall.net/~robotpy/wheels/2023/raspbian/ robotpy-cscore robotpy-apriltag
