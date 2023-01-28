824c618fdf2b07bcbc8f4c41e4bab853412dc5a6
Jonathan Dao <jonathandao0@gmail.com>
2023-01-28 12:21:53 -0800

    Made euler angle calculation on tag_dictionary init

--- aprilTags/tag_dictionary.py
+++ aprilTags/tag_dictionary.py
@@ -1,5 +1,8 @@
 import json
 import logging
+import platform
+
+from common.mathUtils import euler_from_quaternion
 
 log = logging.getLogger(__name__)
 
@@ -8,8 +11,17 @@ File_Path = 'resources/2023-chargedup.json'
 try:
     with open(File_Path) as json_file:
         TAG_DICTIONARY = json.load(json_file)
+        for TAG in TAG_DICTIONARY['tags']:
+            tagQuarternion = TAG['pose']['rotation']['quaternion']
+            pitch, roll, yaw = euler_from_quaternion(tagQuarternion['X'], tagQuarternion['Y'], tagQuarternion['Z'], tagQuarternion['W'])
+            TAG['pose']['rotation']['euler'] = {
+                'pitch': pitch,
+                'roll': roll,
+                'yaw': yaw,
+            }
 
         logging.debug(TAG_DICTIONARY)
 except Exception as e:
     logging.error("Could not open tag dictionary!")
     TAG_DICTIONARY = {}
+
