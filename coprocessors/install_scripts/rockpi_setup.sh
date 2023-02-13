36b1f5735b8d70fa7318449134dbc633eacf74af
Jonathan Dao <jonathandao0@gmail.com>
2023-02-12 21:30:18 -0800

    Updated install scripts

--- coprocessors/install_scripts/rockpi_setup.sh
+++ coprocessors/install_scripts/rockpi_setup.sh
@@ -1,10 +1,9 @@
+echo "" >> ~/.bashrc
+echo "export PYTHONPATH=/home/pi/ChargedUp2023DepthAI:${PYTHONPATH}" >> ~/.bashrc
+source ~/.bashrc
+
 sudo apt-get -y update
-sudo apt-get -y install python-opencv
-sudo apt-get -y install python3-pyqt5
-sudo apt-get -y install usbtop
-sudo apt-get -y install vnstat
-sudo apt-get -y install dos2unix
-sudo apt-get -y install network-manager
-sudo apt-get -y install bridge-utils
+sudo apt-get -y install python-opencv python3-pyqt5 usbtop usbutils vnstat dos2unix network-manager bridge-utils rsync bc dkms
+sudo dpkg -i /opt/linux-headers-*.deb
 sudo wget -qO- https://docs.luxonis.com/install_dependencies.sh | bash
 pip3 install --find-links=https://tortall.net/~robotpy/wheels/2023/raspbian/ robotpy-cscore robotpy-apriltag
