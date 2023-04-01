a9475599b7cbb8da412004d7af95fc09f3ab3823
Matthew Ochoa <matthewochoacoc@gmail.com>
2023-04-01 15:33:51 -0700

    AprilTag Images

--- aprilTags/chatgpt_save_frames.py
+++ aprilTags/chatgpt_save_frames.py
@@ -3,6 +3,8 @@ import cv2
 import os
 import time
 
+import robotpy_apriltag
+
 def save_frame_every_second(output_folder: pathlib.Path, capture_duration:float=60):
     # Ensure the output folder exists
     if not os.path.exists(output_folder):
@@ -20,6 +22,8 @@ def save_frame_every_second(output_folder: pathlib.Path, capture_duration:float=
 
     while int(time.time() - start_time) < capture_duration:
         # Read a frame from the camera
+        image_file_name = input('image filename:')
+
         ret, frame = cap.read()
 
         if not ret:
@@ -29,8 +33,8 @@ def save_frame_every_second(output_folder: pathlib.Path, capture_duration:float=
         current_time = int(time.time())
         if current_time % 1 == 0 and frame_counter != current_time:
             # Save the frame to the output folder
-            output_path = output_folder / f"frame_{frame_counter}.png"
-            cv2.imwrite(output_path, frame)
+            output_path = output_folder / f"{image_file_name}.png"
+            cv2.imwrite(str(output_path), frame)
             print(f"Saved frame {frame_counter} to {output_path}")
             frame_counter += 1
 
@@ -40,7 +44,20 @@ def save_frame_every_second(output_folder: pathlib.Path, capture_duration:float=
     # Release the camera resources
     cap.release()
 
-if __name__ == "__main__":
+## save captured frames
+def loop_save_frames_with_text():
     output_folder = pathlib.Path("captured_frames")
     capture_duration = 60  # Capture images for 60 seconds
     save_frame_every_second(output_folder, capture_duration)
+
+## go through every saved image
+def show_frame():
+    frame_directory = pathlib.Path('captured_frames')
+    for image_file in frame_directory.glob('*.png'):
+        image = cv2.imread(str(image_file))
+        cv2.imshow(str(image_file), image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    show_frame()
