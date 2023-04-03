f72e2818a6b984d33c0cdc46843e8dfd3a2e50fb
Matthew Ochoa <matthewochoacoc@gmail.com>
2023-04-02 17:10:06 -0700

    Detector

--- aprilTags/matthew_tags.py
+++ aprilTags/matthew_tags.py
@@ -5,7 +5,11 @@ import time
 
 import robotpy_apriltag
 
-def save_frame_every_second(output_folder: pathlib.Path, capture_duration:float=60):
+from icecream import ic
+
+import matplotlib.pyplot as plt
+
+def save_frame_every_second(output_folder: pathlib.Path, capture_duration: float = 60):
     # Ensure the output folder exists
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -44,16 +48,18 @@ def save_frame_every_second(output_folder: pathlib.Path, capture_duration:float=
     # Release the camera resources
     cap.release()
 
+
 def loop_save_frames_with_text():
     output_folder = pathlib.Path("captured_frames")
     capture_duration = 60  # Capture images for 60 seconds
     save_frame_every_second(output_folder, capture_duration)
 
+
 def show_frame():
     frame_directory = pathlib.Path('captured_frames')
     for image_file in frame_directory.glob('*.png'):
         image = cv2.imread(str(image_file))
-        image = image[:,:,0]
+        image = image[:, :, 0]
         detector = robotpy_apriltag.AprilTagDetector()
 
         # detector config
@@ -68,21 +74,44 @@ def show_frame():
         # print detections
         detector.addFamily("tag16h5", 3)
         detections = detector.detect(image)
-        print(detections)
 
-        # cycles through every detected tag and prints detection corners
-        if len(detections) > 0:
-            for tag in detections:
-                tagCorners = [tag.getCorner(0), tag.getCorner(1), tag.getCorner(2), tag.getCorner(3)]
-                print(tagCorners)
-                continue
-
-        # 4 - 1 right side; 3 - 2 left side; right side - left side = if tag is usable
+        # calls tag filter
+        tag_filter(detections)
 
         # cycle images
-        cv2.imshow(str(image_file), image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        plt.imshow(image, cmap='gray', vmin=0, vmax=image.max())
+        plt.show()
+        # cv2.imshow(str(image_file), image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+
+def tag_filter(detections):
+    # cycles through every detected tag and prints detection corners
+    # 3 - 0 right side; 2 - 1 left side; right side - left side = if tag is usable
+    tag_threshold = 10
+
+    if len(detections) > 0:
+        for tag in detections:
+            tag_corners = [tag.getCorner(0), tag.getCorner(1), tag.getCorner(2), tag.getCorner(3)]
+
+            right_side = point_dist(tag_corners[3], tag_corners[0])
+            left_side = point_dist(tag_corners[2], tag_corners[1])
+
+            side_diff = abs(right_side - left_side)
+
+            if side_diff < tag_threshold:
+                continue
+            else:
+                ic(tag_corners)
+                ic(side_diff)
+                print(tag)
+                continue
+
+
+def point_dist(p1: robotpy_apriltag.AprilTagDetection.Point, p2: robotpy_apriltag.AprilTagDetection.Point):
+    return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+
 
 if __name__ == "__main__":
     show_frame()
