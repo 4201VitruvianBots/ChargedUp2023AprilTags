1c10fc39084b88b949b3945ba1bd6d452bd7edb3
Unknown <jonathandao0@gmail.com>
2023-04-11 00:32:59 -0700

    Added Northstar

--- cscore_utils/CVCamera.py
+++ cscore_utils/CVCamera.py
@@ -27,23 +27,27 @@ class CVCamera:
         log.info("Setting up camera settings...")
         if platform.system() == 'Windows':
             self.cap = cv2.VideoCapture(deviceId)
-        else:
-            self.cap = cv2.VideoCapture("v4l2src device=/dev/video{} ! video/x-raw, width={}, height={} ! videoconvert ! video/x-raw,format=GRAY ! appsink".format(deviceId,
-                                                                                                                                                                   camera_params['width'],
-                                                                                                                                                                   camera_params['height']))
 
-        self.cap.set(cv2.CAP_PROP_FPS, camera_params['fps'])
-        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 0)
-        self.cap.set(cv2.CAP_PROP_SHARPNESS, 3)
-        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
-        self.cap.set(cv2.CAP_PROP_EXPOSURE, 157)
-        self.cap.set(cv2.CAP_PROP_GAIN, 0)
-        self.cap.set(cv2.CAP_PROP_SHARPNESS, 3)
-        self.cap.set(cv2.CAP_PROP_AUTO_WB, 1)
-        self.cap.set(cv2.CAP_PROP_CONTRAST, 32)
-        self.cap.set(cv2.CAP_PROP_GAMMA, 100)
-        self.cap.set(cv2.CAP_PROP_HUE, 0)
-        self.cap.set(cv2.CAP_PROP_SATURATION, 64)
+            self.cap.set(cv2.CAP_PROP_FPS, camera_params['fps'])
+            self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 0)
+            self.cap.set(cv2.CAP_PROP_SHARPNESS, 3)
+            self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+            self.cap.set(cv2.CAP_PROP_EXPOSURE, 157)
+            self.cap.set(cv2.CAP_PROP_GAIN, 0)
+            self.cap.set(cv2.CAP_PROP_SHARPNESS, 3)
+            self.cap.set(cv2.CAP_PROP_AUTO_WB, 1)
+            self.cap.set(cv2.CAP_PROP_CONTRAST, 32)
+            self.cap.set(cv2.CAP_PROP_GAMMA, 100)
+            self.cap.set(cv2.CAP_PROP_HUE, 0)
+            self.cap.set(cv2.CAP_PROP_SATURATION, 64)
+        else:
+            self.cap = cv2.VideoCapture("v4l2src device=/dev/video" + str(deviceId) +
+                                        " extra_controls=\"c,exposure_auto=" + str(1) +
+                                        ",exposure_absolute=" + str(157) +
+                                        ",gain=" + str(0) + ",sharpness=0,brightness=0\"" +
+                                        "! image/jpeg,format=MJPG,width=" + str(camera_params['width']) +
+                                        ",height=" + str(camera_params['height']) +
+                                        " ! jpegdec ! video/x-raw ! appsink drop=1", cv2.CAP_GSTREAMER)
 
         self.frame = np.zeros([camera_params['height'], camera_params['width'], 3], dtype=np.uint8)
 
